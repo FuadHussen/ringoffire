924b0472b2fc974adc8a361a89570be350cc1e1f
FuadHussen <142115071+FuadHussen@users.noreply.github.com>
2024-11-10 18:06:45 +0100

    added new c game

--- src/app/start-screen/start-screen.component.ts
+++ src/app/start-screen/start-screen.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-start-screen',
   standalone: true,
@@ -9,4 +9,9 @@ import { Component } from '@angular/core';
 })
 export class StartScreenComponent {
 
+  constructor(private router: Router) { }
+
+  newGame() {
+    this.router.navigate(['/game']);
+  }
 }
