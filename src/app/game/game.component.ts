a1cfec7743dc68c6b20d26ad95a23f00645e3b60
FuadHussen <142115071+FuadHussen@users.noreply.github.com>
2024-11-11 13:12:38 +0100

    added new c game & player & added animation for cards

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,12 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Game } from '../../models/game';
+import { PlayerComponent } from '../player/player.component';
 
 @Component({
   selector: 'app-game',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, PlayerComponent],
   templateUrl: './game.component.html',
   styleUrl: './game.component.scss'
 })
-export class GameComponent {
+export class GameComponent implements OnInit {
+  pickCardAnimation = false;
+  currentCard: string = '';
+  playedCards: string[] = [];
+  game: Game;
 
+  constructor() {
+    this.game = new Game();
+  }
+
+  ngOnInit(): void {
+    this.newGame();
+  }
+
+  newGame() {
+    this.game = new Game();
+    console.log(this.game);
+  }
+
+  pickCard() {
+    if (!this.pickCardAnimation) {
+      let card = this.game.stack.pop();
+      this.currentCard = card ?? '';
+      this.pickCardAnimation = true;
+
+      setTimeout(() => {
+        this.game.playedCard.push(this.currentCard);
+        this.pickCardAnimation = false;
+      }, 1000);
+    }
+  }
 }
